af2bb5d1542d5b38ff0d3ac0a2186722d8d982e2
Kürşad Bumin Giray Saka <kbgs.1903@gmail.com>
2023-09-05 23:54:17 +0300

    Initial Commit

---
+++ LinkedInUi/LinkedInUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedInUiApp.swift
+//  LinkedInUi
+//
+//  Created by Kürşad Saka on 5.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LinkedInUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
